2b8a8074e4bd301f626362d2a8d61a33e80ada38
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2025-07-06 03:42:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
+
+if ($text == "") {
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    $response = "Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+
+} else if ($text == "2") {
+    $response = "END Your phone number is ".$phoneNumber;
+} else if($text == "1*1") { 
+    $accountNumber  = "ACC1001";
+    $response = "END Your account number is ".$accountNumber;
+}
+
+header('Content-type: text/plain');
+echo $response;
